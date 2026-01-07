424035b79570e029fcf95246acbeeae4c7b6527b
Gabriela Constante de Oliveira <gabriela.oliveira@unimedsc.coop.br>
2026-01-07 09:16:39 -0300

    Alteraçãoes nas macros

--- macros/gerar_chave_incremental.sql
+++ macros/gerar_chave_incremental.sql
@@ -4,32 +4,26 @@
    
    Parâmetros:
    - colunas_ordenacao: Lista de colunas para ordenação
-   - nome_coluna: Nome da coluna de ID (padrão: 'id')
+   - nome_coluna: Nome da coluna de ID (padrão: 'CD_CHAVE_IDENTIFICACAO')
    - tabela_destino: Tabela de destino (padrão: this)
-   - prefixo: Prefixo numérico (ex: 10, 2) - *Opcional*
 ============================================================================ #}
-{% macro gerar_chave_incremental(
-    colunas_ordenacao,
-    nome_coluna='id',
-    tabela_destino=this,
-    prefixo=none
-) %}
+{% macro gerar_chave_incremental(colunas_ordenacao, nome_coluna='CD_CHAVE_IDENTIFICACAO', tabela_destino=this) %}
     {% set ordem = colunas_ordenacao | join(', ') %}
     
     {% if is_incremental() %}
-        {% set max_id_query %}
-            SELECT NVL(MAX({{ nome_coluna }}), {% if prefixo %}{{ prefixo }}{% else %}0{% endif %}) AS max_id
+        {% set maior_cd_tabela %}
+            SELECT NVL(MAX({{ nome_coluna }}), 0) AS maior_cd
             FROM {{ tabela_destino }}
         {% endset %}
         
-        {% set resultado = run_query(max_id_query) %}
-        {% set max_id = resultado.columns[0].values()[0] if resultado and resultado.columns[0].values() | length > 0 else (prefixo if prefixo else 0) %}
+        {% set resultado = run_query(maior_cd_tabela) %}
+        {% set maior_cd = resultado.columns[0].values()[0]
+           if resultado and resultado.columns[0].values() | length > 0
+           else 0
+        %}
         
         {{ max_id }} + ROW_NUMBER() OVER (ORDER BY {{ ordem }}) AS {{ nome_coluna }}
     {% else %}
-        {% if prefixo %}
-            {{ prefixo }} + ROW_NUMBER() OVER (ORDER BY {{ ordem }}) AS {{ nome_coluna }}
-        {% else %}
             ROW_NUMBER() OVER (ORDER BY {{ ordem }}) AS {{ nome_coluna }}
         {% endif %}
     {% endif %}
